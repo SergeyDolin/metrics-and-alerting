852672f11830f6b6620472bb4676f6ab63067198
Sergei Dolin <thewarlusgray@gmail.com>
2025-09-02 15:41:32 +0700

    unit tests for server and agent

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,114 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"runtime"
+	"time"
+)
+
+type MetricStorage struct {
+	gauge   map[string]float64
+	counter map[string]int64
+}
+
+func createMetricStorage() *MetricStorage {
+	return &MetricStorage{
+		gauge:   make(map[string]float64),
+		counter: make(map[string]int64),
+	}
+}
+
+func (ms *MetricStorage) getMetrics(m *runtime.MemStats) {
+	ms.gauge["Alloc"] = float64(m.Alloc)
+	ms.gauge["BuckHashSys"] = float64(m.BuckHashSys)
+	ms.gauge["Frees"] = float64(m.Frees)
+	ms.gauge["GCCPUFraction"] = float64(m.GCCPUFraction)
+	ms.gauge["GCSys"] = float64(m.GCSys)
+	ms.gauge["HeapAlloc"] = float64(m.HeapAlloc)
+	ms.gauge["HeapIdle"] = float64(m.HeapIdle)
+	ms.gauge["HeapObjects"] = float64(m.HeapObjects)
+	ms.gauge["HeapReleased"] = float64(m.HeapReleased)
+	ms.gauge["HeapSys"] = float64(m.HeapSys)
+	ms.gauge["LastGC"] = float64(m.LastGC)
+	ms.gauge["Lookups"] = float64(m.Lookups)
+	ms.gauge["MCacheInuse"] = float64(m.MCacheInuse)
+	ms.gauge["MCacheSys"] = float64(m.MCacheSys)
+	ms.gauge["MSpanSys"] = float64(m.MSpanSys)
+	ms.gauge["Mallocs"] = float64(m.Mallocs)
+	ms.gauge["NextGC"] = float64(m.NextGC)
+	ms.gauge["NumForcedGC"] = float64(m.NumForcedGC)
+	ms.gauge["NumGC"] = float64(m.NumGC)
+	ms.gauge["OtherSys"] = float64(m.OtherSys)
+	ms.gauge["PauseTotalNs"] = float64(m.PauseTotalNs)
+	ms.gauge["StackInuse"] = float64(m.StackInuse)
+	ms.gauge["Sys"] = float64(m.Sys)
+	ms.gauge["TotalAlloc"] = float64(m.TotalAlloc)
+	ms.counter["PollCount"]++
+	ms.gauge["RandomValue"] = rand.Float64()
+}
+
+func sendMetric(name, typeMetric string, value string, serverAddr string) error {
+	// http://<АДРЕС_СЕРВЕРА>/update/<ТИП_МЕТРИКИ>/<ИМЯ_МЕТРИКИ>/<ЗНАЧЕНИЕ_МЕТРИКИ>
+	url := fmt.Sprintf("http://%s/update/%s/%s/%s", serverAddr, typeMetric, name, value)
+
+	req, err := http.NewRequest(http.MethodPost, url, nil)
+	if err != nil {
+		return fmt.Errorf("request error: %v", err)
+	}
+	req.Header.Set("Content-Type", "text/plain")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("response error: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("server return code %d", resp.StatusCode)
+	}
+	return nil
+}
+
+func main() {
+	ms := createMetricStorage()
+
+	serverAddr := "localhost:8080"
+
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+
+	pollInterval := 2 * time.Second
+	reportInterval := 10 * time.Second
+	lastReport := time.Now()
+
+	ms.getMetrics(&m)
+	for {
+		time.Sleep(pollInterval)
+		ms.getMetrics(&m)
+
+		if time.Since(lastReport) >= reportInterval {
+			sent := 0
+			for name, value := range ms.gauge {
+				err := sendMetric(name, "gauge", fmt.Sprintf("%f", value), serverAddr)
+				if err != nil {
+					fmt.Printf("error send gauge %v", err)
+				} else {
+					sent++
+				}
+			}
+			for name, value := range ms.counter {
+				err := sendMetric(name, "counter", fmt.Sprintf("%d", value), serverAddr)
+				if err != nil {
+					fmt.Printf("error send counter %v", err)
+				} else {
+					sent++
+				}
+			}
+			lastReport = time.Now()
+		}
+	}
+
+}
---
+++ cmd/server/main_test.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_postHandler(t *testing.T) {
+	ms := createMetricStorage()
+	handler := postHandler(ms) // Получаем обработчик
+
+	tests := []struct {
+		name            string
+		method          string
+		contentType     string
+		url             string
+		expectedStatus  int
+		expectedBody    string
+		expectedGauge   map[string]float64
+		expectedCounter map[string]int64
+	}{
+		{
+			name:            "Valid gauge update",
+			method:          http.MethodPost,
+			contentType:     "text/plain",
+			url:             "/update/gauge/temp/25.5",
+			expectedStatus:  http.StatusOK,
+			expectedGauge:   map[string]float64{"temp": 25.5},
+			expectedCounter: map[string]int64{},
+		},
+		{
+			name:            "Valid counter update",
+			method:          http.MethodPost,
+			contentType:     "text/plain",
+			url:             "/update/counter/req/10",
+			expectedStatus:  http.StatusOK,
+			expectedGauge:   map[string]float64{},
+			expectedCounter: map[string]int64{"req": 10},
+		},
+		{
+			name:            "Counter increment twice",
+			method:          http.MethodPost,
+			contentType:     "text/plain",
+			url:             "/update/counter/hits/7",
+			expectedStatus:  http.StatusOK,
+			expectedCounter: map[string]int64{"hits": 7},
+		},
+		{
+			name:           "Invalid method",
+			method:         http.MethodGet,
+			contentType:    "text/plain",
+			url:            "/update/gauge/temp/1.0",
+			expectedStatus: http.StatusMethodNotAllowed,
+			expectedBody:   "Only POST request allowed!",
+		},
+		{
+			name:           "Invalid content type",
+			method:         http.MethodPost,
+			contentType:    "application/json",
+			url:            "/update/gauge/temp/1.0",
+			expectedStatus: http.StatusBadRequest,
+			expectedBody:   "Invalid Content-Type",
+		},
+		{
+			name:           "Invalid path format",
+			method:         http.MethodPost,
+			contentType:    "text/plain",
+			url:            "/update/gauge/temp",
+			expectedStatus: http.StatusNotFound,
+			expectedBody:   "Invalid path format",
+		},
+		{
+			name:           "Unknown metric type",
+			method:         http.MethodPost,
+			contentType:    "text/plain",
+			url:            "/update/unknown/test/123",
+			expectedStatus: http.StatusBadRequest,
+			expectedBody:   "Unknown metric type",
+		},
+		{
+			name:           "Invalid gauge value",
+			method:         http.MethodPost,
+			contentType:    "text/plain",
+			url:            "/update/gauge/temp/abc",
+			expectedStatus: http.StatusBadRequest,
+			expectedBody:   "Only Float type for Gauge allowed!",
+		},
+		{
+			name:           "Invalid counter value",
+			method:         http.MethodPost,
+			contentType:    "text/plain",
+			url:            "/update/counter/req/xyz",
+			expectedStatus: http.StatusBadRequest,
+			expectedBody:   "Only Int type for Counter allowed!",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req := httptest.NewRequest(tt.method, tt.url, nil)
+			req.Header.Set("Content-Type", tt.contentType)
+
+			rr := httptest.NewRecorder()
+			handler(rr, req)
+
+			assert.Equal(t, tt.expectedStatus, rr.Code, "HTTP status not equal")
+
+			body := rr.Body.String()
+			assert.Contains(t, body, tt.expectedBody, "Body not include expected text")
+
+			for name, value := range tt.expectedGauge {
+				got, exists := ms.gauge[name]
+				assert.True(t, exists, "Expect metric gauge %s", name)
+				assert.Equal(t, value, got, "Value gauge %s not contain", name)
+			}
+
+			for name, value := range tt.expectedCounter {
+				got, exists := ms.counter[name]
+				assert.True(t, exists, "Expect metric counter %s", name)
+				assert.Equal(t, value, got, "Value counter %s not contain", name)
+			}
+		})
+	}
+}
