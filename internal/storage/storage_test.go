66c6cbac0a28767e763ab09c226d87f28b486808
Sergei Dolin <thewarlusgray@gmail.com>
2026-03-02 08:57:48 +0700

    fix bugs

--- internal/storage/storage_test.go
+++ internal/storage/storage_test.go
@@ -5,18 +5,18 @@ import (
 	"testing"
 )
 
-func BenchmarkMemStorageUpdateGauge(b *testing.B, ctx context.Context) {
+func BenchmarkMemStorageUpdateGauge(b *testing.B) {
 	storage := NewMemStorage()
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		storage.UpdateGauge(ctx, "test", float64(i))
+		storage.UpdateGauge(context.Background(), "test", float64(i))
 	}
 }
 
-func BenchmarkMemStorageGetAll(b *testing.B, ctx context.Context) {
+func BenchmarkMemStorageGetAll(b *testing.B) {
 	storage := NewMemStorage()
 	for i := 0; i < 100; i++ {
-		storage.UpdateGauge(ctx, "test", float64(i))
+		storage.UpdateGauge(context.Background(), "test", float64(i))
 	}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
